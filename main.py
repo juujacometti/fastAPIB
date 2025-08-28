0dd20e079a44576a744f14ccb61bba148c9dc947
juujacometti <julyajacometty@gmail.com>
2025-08-28 10:00:49 -0300

    Aula 01 e 02 FastAPI

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
+# ====== Explicação dos imports ====== 
+    # FastAPI → Classe principal para criar a aplicação.
+    # HTTPException → Usada para lançar erros (ex: 404 Not Found).
+    # status → Contém códigos HTTP prontos (200, 201, 404...).
+    # Response → Permite personalizar respostas HTTP.
+    # Depends → Injeta dependências em endpoints (ex: conexão com banco).
+    # Models → Importa o modelo de dados para validar e tipar os personagens.
+    # Optional → Indica que um parâmetro pode ser opcional.
+    # Any → Aceita qualquer tipo de dado.
+    # Routes → Importa rotas organizadas em outros arquivos
+    # Requests → Biblioteca para fazer requisições HTTP externas
 from fastapi import FastAPI, HTTPException, status, Response, Depends
 from models import PersonagensToyStory
 from typing import Optional, Any
 from routes import curso_router, usuario_router
 import requests
 
+# Criação da aplicação - Instancia a aplicação adicionando metadados (título, versão e descrição)
 app = FastAPI(title="API dos personagens de Toy Story - DS18", version="0.0.1", description="API feita com a DS18 para aprender FastAPI")
 
-app.include_router(curso_router.router, tags=["Cursos"])
+# Rotas externas que foram definidas em outros arquivos
+app.include_router(curso_router.router, tags=["Cursos"])    # As tags ajudam a organizar a documentação '/docs'
 app.include_router(usuario_router.router, tags=["Usuários"])
 
 # Utilizando API externa
-@app.get("/pokemon/{name}")
-def get_pokemon(name: str):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+@app.get("/pokemon/{name}")    # @ - Decorator: registra uma rota HTTP GET em /pokemon/<algum_nome>
+def get_pokemon(name: str):    # Função que atende a rota; 'name' é o parâmetro de caminho
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")    # Chamada HTTP síncrona usando 'requests' para consultar a PokeAPI
+    
+    # Caso o pokemon exista, o código retorna o json vindo da PokeAPI
     if response.status_code == 200:
         return response.json()
-    return{"Message": "Pokemon not found"}
+    return{"Message": "Pokemon not found"}    # Caso não exista, retorna a mensagem de erro
 
+# Simulação de um banco de dados  (é usada como dependência com o Depends)
 def fake_db():
     try:
         print("Conectando com o banco")
     finally:
         print("Fechando o banco")
 
+# "Dados em memória" - Diconário que guarda os personagens como se fosse um banco de dados
 personagens = {
     1: {
         "nome": "Woody",
@@ -38,31 +55,34 @@ personagens = {
     }
 }
 
-# @ - É um decorator. 
-@app.get("/")
+# Endpoints
+@app.get("/")    # Rota raíz 
 async def raiz():
-    return {"Mensagem": "Deu certo"}
+    return {"Mensagem": "Deu certo"}    # retorna mensagem de confirmação
 
-@app.get("/personagens")
-async def get_personagens(db: Any = Depends(fake_db)):
-    return personagens
+@app.get("/personagens")    
+async def get_personagens(db: Any = Depends(fake_db)):    # 'Depends(fake_db) executa a função 'fake_db()' sempre que esse endpoint for chamado 
+    return personagens    # Retorna todos os personagens
 
-@app.get("/personagens/{personagem_id}")
+# Retorna um personagem específico pelo id 
+@app.get("/personagens/{personagem_id}")    
 async def get_personagem(personagem_id: int):
     try:
         personagem = personagens[personagem_id]
         return personagem
-    except KeyError:
+    except KeyError:    # Caso o personagem não exista, retorna o erro 404
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
-    
-@app.post("/personagens", status_code=status.HTTP_201_CREATED)
-async def post_personagem(personagem: Optional[PersonagensToyStory] = None):
-    next_id = len(personagens) + 1
-    personagens[next_id] = personagem
+
+# Cria um novo personagem
+@app.post("/personagens", status_code=status.HTTP_201_CREATED)    # Registra uma rota HTTP POST no caminho /personagens.
+async def post_personagem(personagem: Optional[PersonagensToyStory] = None):    
+    next_id = len(personagens) + 1    # Calcula um novo ID simples contando quantos itens existem no dicionário personagens e somando 1
+    personagens[next_id] = personagem    # Insere no dicionário personagens
     del personagem.id
     return personagem
 
-@app.put("/personagens/{personagem_id}", status_code=status.HTTP_202_ACCEPTED)
+# Atualiza um personagem existente pelo id
+@app.put("/personagens/{personagem_id}", status_code=status.HTTP_202_ACCEPTED)    
 async def put_personagem(personagem_id:int, personagem: PersonagensToyStory):
     if personagem_id in personagens:
         personagens[personagem_id] = personagem
